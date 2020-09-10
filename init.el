e4331d22a5e4ed2a5c32ea2ee2221ecc1ae148d2
Otávio Schwanck <otavioschwanck@gmail.com>
2020-09-10 11:00:34 -0300

    add docker

--- init.el
+++ init.el
@@ -86,7 +86,7 @@
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
