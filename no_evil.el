9f23437fcfd4db190bdbfd7b1a2651669e1cd831
Otávio Schwanck <otavioschwanck@gmail.com>
2020-09-17 18:57:05 -0300

    duplicate lines, oh yea

--- no_evil.el
+++ no_evil.el
@@ -29,6 +29,30 @@
 (map! "M-p" #'drag-stuff-up)
 (map! "M-n" #'drag-stuff-down)
 
+(defun duplicate-line-or-region (&optional n)
+  "Duplicate current line, or region if active.
+    With argument N, make N copies.
+    With negative N, comment out original line and use the absolute value."
+  (interactive "*p")
+  (let ((use-region (use-region-p)))
+    (save-excursion
+      (let ((text (if use-region        ;Get region if active, otherwise line
+                      (buffer-substring (region-beginning) (region-end))
+                    (prog1 (thing-at-point 'line)
+                      (end-of-line)
+                      (if (< 0 (forward-line 1)) ;Go to beginning of next line, or make a new one
+                          (newline))))))
+        (dotimes (i (abs (or n 1)))     ;Insert N times, or once if not specified
+          (insert text))))
+    (if use-region nil                  ;Only if we're working with a line (not a region)
+      (let ((pos (- (point) (line-beginning-position)))) ;Save column
+        (if (> 0 n)                             ;Comment out original with negative arg
+            (comment-region (line-beginning-position) (line-end-position)))
+        (forward-char pos)))))
+(forward-line 1)
+
+(map! "M-N" #'duplicate-line-or-region)
+
 ;; (define-key org-mode-map (kbd "C-c l <return>") #'+org/dwim-at-point)
 
 (defun save-all ()
