de6b7aaf867c0963a8282d50b39daf476ee46ce9
Otávio Schwanck <otavioschwanck@gmail.com>
2020-11-12 21:51:27 -0300

    set company on inf-ruby

--- company.el
+++ company.el
@@ -47,8 +47,10 @@
 (use-package! company-tabnine)
 
 (after! robe
-  (set-company-backend! 'inf-ruby-mode 'compay-tabnine 'company-dabbrev-code 'company-capf 'company-yasnippet)
   (set-company-backend! 'ruby-mode 'company-tabnine 'company-capf 'company-dabbrev-code 'company-yasnippet))
+
+(after! inf-ruby
+  (set-company-backend! 'inf-ruby-mode 'compay-tabnine 'company-dabbrev-code 'company-capf 'company-yasnippet))
 
 (after! tide
   (set-company-backend! 'js2-mode 'company-tabnine 'company-tide 'company-capf 'company-dabbrev-code 'company-yasnippet))
