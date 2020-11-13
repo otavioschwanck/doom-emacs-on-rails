26563f7d58accae1cba00d9bf5bf9e24d3196834
Otávio Schwanck <otavioschwanck@gmail.com>
2020-11-12 21:39:30 -0300

    welcome back tabnine

--- company.el
+++ company.el
@@ -10,9 +10,13 @@
   (setq company-idle-delay 0))
 
 (after! robe
-  (set-company-backend! 'ruby-mode 'company-dabbrev-code 'company-capf 'company-yasnippet))
+  (set-company-backend! 'inf-ruby-mode 'company-tabnine 'company-dabbrev-code 'company-capf 'company-yasnippet)
+  (set-company-backend! 'ruby-mode 'company-tabnine 'company-capf 'company-dabbrev-code 'company-yasnippet))
+
+(after! js2-mode
+  (set-company-backend! 'js2-mode 'company-tabnine 'company-tide 'company-dabbrev-code 'company-yasnippet))
 
 (add-hook! 'lsp-completion-mode-hook
-  (defun init-company-dabbrev-code-h ()
+  (defun init-company-tabnine-h ()
     (when lsp-completion-mode
-      (setq-local company-backends (cons 'company-dabbrev-code company-backends)))))
+      (setq-local company-backends (cons 'company-tabnine company-backends)))))
