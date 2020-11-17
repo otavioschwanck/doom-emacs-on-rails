3e795d91fdad015389e7ce1cdb604d5778fd860f
Otávio Schwanck <otavioschwanck@gmail.com>
2020-11-17 15:02:30 -0300

    performance improvements

--- ruby.el
+++ ruby.el
@@ -110,7 +110,7 @@
 (map! :mode ruby-mode-map :leader "A" 'goto-test-and-vsplit)
 
 (with-eval-after-load 'flycheck
-  (setq-default flycheck-disabled-checkers '(ruby-reek)))
+  (setq-default flycheck-disabled-checkers '(ruby-reek lsp)))
 
 (defun otavio/chomp (str)
   "Trim leading and trailing whitespace from STR."
