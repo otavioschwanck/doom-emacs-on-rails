05aa75bb6749aec4fab809cead33d569a100f665
Otávio Schwanck <otavioschwanck@gmail.com>
2020-11-20 13:03:39 -0300

    improve readme and deft

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
        ;;ido               ; the other *other* search engine...
        (ivy +fuzzy +prescient); a search engine for love and life
        :ui
-       ;;deft              ; notational velocity for Emacs
+       deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
