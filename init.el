04c2c42de89419a6625c3c426123f3f7b00f425f
Otávio Schwanck <otavioschwanck@gmail.com>
2020-11-25 10:04:29 -0300

    use tide instead LSP

--- init.el
+++ init.el
@@ -133,7 +133,7 @@
        ;;idris             ;
        ;;json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)          ; all(hope(abandon(ye(who(enter(here))))))
+       javascript          ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
