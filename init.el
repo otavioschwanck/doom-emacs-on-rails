162d20581876b5a9f3d4ee6f60f575ddd95430e5
Otávio Schwanck <otavioschwanck@gmail.com>
2020-11-26 18:26:07 -0300

    remove childframe

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company +childframe)           ; the ultimate code completion backend
+       company             ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy +fuzzy +prescient); a search engine for love and life
