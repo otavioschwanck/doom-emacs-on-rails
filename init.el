261a2b6e826690f8587079305eafd36f88832a91
Otávio Schwanck <otavioschwanck@gmail.com>
2020-12-01 22:43:23 -0300

    childframe for SPC SPC

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
        company             ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +fuzzy +prescient); a search engine for love and life
+       (ivy +fuzzy +prescient +childframe); a search engine for love and life
 
        :ui
        deft              ; notational velocity for Emacs
