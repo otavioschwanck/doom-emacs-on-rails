bae98615d1315ca917e14b22214250d014c3d8d4
Otávio Schwanck <otavioschwanck@gmail.com>
2020-12-03 00:40:55 -0300

    use emmet expand instead emmet expand yas

--- editor.el
+++ editor.el
@@ -102,7 +102,7 @@
 ;; C-w C-w evil next > other-window
 (map! :map evil-window-map "C-w" #'evil-window-next)
 (map! "C-<SPC>" #'evil-window-next)
-(map! :after web-mode :map web-mode-map :i "C-e" #'emmet-expand-yas)
+(map! :after web-mode :map web-mode-map :i "C-e" #'emmet-expand)
 
 (setq lsp-enable-file-watchers nil)
 
