5468fd2ceabd04df619d6a338e87377eec403e8c
Otávio Schwanck dos Santos <otavio@MacBook-Air-de-Otavio.local>
2021-05-11 14:23:13 -0300

    add indent guides

--- init.el
+++ init.el
@@ -33,7 +33,7 @@
        fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
-       ;; indent-guides     ; highlighted indent columns
+       indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline            ; snazzy, Atom-inspired modeline, plus API
