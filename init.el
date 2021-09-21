05349fc217b23605809a5017624a4de8c669d997
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2021-09-21 01:20:07 -0300

    remove tabs

--- init.el
+++ init.el
@@ -42,7 +42,7 @@
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       tabs              ; a tab bar for Emacs
+       ;; tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
