f242294dd86c4a68f5d746c3adce76df6fdb70c4
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2021-09-30 15:44:01 -0300

    hello again forge

--- init.el
+++ init.el
@@ -94,7 +94,7 @@
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp               ; M-x vscode
-       magit             ; a git porcelain for Emacs
+       (magit +forge)             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        pdf               ; pdf enhancements
