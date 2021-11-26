92bdcf5d6a89625d9a6e7b6ef1035fb30dd4ae2a
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2021-11-26 18:44:07 -0300

    use only capf for ruby

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
-       undo              ; persistent, smarter undo for your inevitable mistakes
+       (undo +tree)              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
