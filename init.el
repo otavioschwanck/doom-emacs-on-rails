d707789f622575016b3bceeb53eccf7d5ea8ad19
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2021-11-28 05:02:13 -0300

    improve company for org and text mode

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       ;;(spell +flyspell) ; tasing you for misspelling mispelling
+       spell              ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
