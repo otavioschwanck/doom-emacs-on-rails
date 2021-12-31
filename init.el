b8371dcc879938ab0c11584a02d33ade9c2b6476
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2021-12-31 18:40:44 -0300

    Upgrade handbook

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
