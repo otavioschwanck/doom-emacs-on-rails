2588d3663406cdc57ad2c1c76ece1ce33b46123f
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-01-05 03:48:51 -0300

    tmux module

--- init.el
+++ init.el
@@ -102,7 +102,7 @@
        rgb               ; creating color strings
        taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
-       ;;tmux              ; an API for interacting with tmux
+       tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
