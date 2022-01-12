793e3a26ce54ce780a0cea0646e9f8f50f8e2787
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-01-12 00:10:57 -0300

    fix harpoon select

--- plugins/harpoon.el
+++ plugins/harpoon.el
@@ -169,7 +169,7 @@
   (let ((result (completing-read "Harpoon to file: "
                                  (delete (s-replace-regexp (projectile-project-p) "" (or (buffer-file-name) ""))
                                          (delete "" (split-string (harpoon--get-file-text) "\n"))))))
-    (when result
+    (when (and result (not (string-equal result "")))
       (find-file (concat (projectile-project-p) result)))))
 
 (define-derived-mode harpoon-mode nil "Harpoon"
