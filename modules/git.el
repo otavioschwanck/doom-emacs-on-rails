f335cc0eff2ceeaced30569f8cd77364ad60e849
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-05-29 23:20:59 +0100

    visit / create PR

--- modules/git.el
+++ modules/git.el
@@ -13,4 +13,69 @@
   (setq magit-diff-highlight-trailing nil)
   (setq magit-diff-paint-whitespace nil)
   (setq magit-diff-highlight-hunk-body nil)
-  (setq magit-diff-refine-hunk nil))
+  (setq magit-diff-refine-hunk nil)
+
+  (defconst endless/gitlab-pr-format
+    (concat "https://gitlab.com/%s/merge_requests/new?"
+            "merge_request[target_branch]=%s&"
+            "merge_request[source_branch]=%s"
+            "%s")
+    "Merge request URL format for GitLab.")
+
+  (defconst endless/github-pr-format
+    "https://github.com/%s/compare/%s...%s?expand=1&assignee=Malabarba%s"
+    "Pull request URL format for GitHub.")
+
+  (defvar endless/pr-body-format nil)
+
+  (defvar endless/pr-additional-params "")
+
+  (defun endless/issue-md-link-from-id (id)
+    (format "[%s](https://atlassian.net/browse/%s)"
+            id id))
+
+  (defun endless/branch-to-title-and-link (branch)
+    "Return a title string derived from current branch."
+    (when (string-match "\\`\\([[:alpha:]]+\\(?:-[0-9]+\\)\\)-\\(.*\\)\\'" branch)
+      (let ((prefix (upcase (match-string 1 branch)))
+            (title (match-string 2 branch)))
+        (let* ((case-fold-search nil)
+               (spaced (replace-regexp-in-string "-+" " " title))
+               (capitalized (if (string-match "[A-Z]" spaced) spaced (capitalize spaced))))
+          `((title . ,(format "[%s] %s" prefix capitalized))
+            (link . ,(endless/issue-md-link-from-id prefix)))))))
+
+  (defun endless/visit-pull-request-url ()
+    "Visit the current branch's PR on Github."
+    (interactive)
+    (let* ((base-branch (replace-regexp-in-string
+                         "^origin/" ""
+                         (or (magit-get-upstream-branch) "master")))
+           (upstream "origin")
+           (remote-url (magit-get "remote" upstream "url"))
+           (current-branch (magit-get-current-branch))
+           ;; (branch-desc (magit-get "branch" current-branch "description"))
+           )
+      (browse-url
+       (format
+        (if (string-match "gitlab\\.com:" remote-url)
+            endless/gitlab-pr-format
+          endless/github-pr-format)
+        (replace-regexp-in-string
+         "\\`.+git...\\.com:\\(.+\\)\\.git\\'" "\\1"
+         remote-url)
+        base-branch
+        current-branch
+        (let-alist (endless/branch-to-title-and-link current-branch)
+          (let ((body-param (when endless/pr-body-format
+                              (thread-last .link
+                                (format endless/pr-body-format)
+                                (url-encode-url)
+                                (replace-regexp-in-string "#" "%23")
+                                (concat "&body="))))
+                (title-param (when .title
+                               (concat "&title=" (url-encode-url .title)))))
+            (when (and .link (not body-param))
+              (kill-new .link))
+            (concat endless/pr-additional-params title-param body-param)))))))
+  (map! :map magit-status-mode-map ";" #'endless/visit-pull-request-url))
